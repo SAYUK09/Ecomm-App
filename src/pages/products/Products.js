0690d60b3ba4c96371460e838578c86c15fbc827
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-05-25 00:25:59 +0530

    Refract : Made a Separate Sidebar Component

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -6,6 +6,7 @@ import { useWishlist } from "../../contexts/Wishlist-Context";
 import { addToCart } from "../../pages/cart/Cart";
 import { axiosAddToCart, axiosAddQty } from "../../utilty/cart-utility";
 import { axiosAddToWishlist } from "../../utilty/wishlist-utility";
+import { Sidebar } from "../../components/sidebar/sidebar";
 
 export function Products() {
   const { state, dispatch } = useProductContext();
@@ -42,56 +43,7 @@ export function Products() {
     <>
       <div className="productsParent">
         <aside>
-          <div
-            className="btnPrimary"
-            onClick={() => {
-              dispatch({ type: "RESET" });
-            }}
-          >
-            RESET
-          </div>
-          <div className="asideTitle">SORT BY</div>
-          <div className="radioBtnDiv">
-            <label>
-              <input
-                type="radio"
-                name="sort"
-                value={state.sortBy}
-                onChange={() =>
-                  dispatch({ type: "SORT", payload: "PRICE_HIGH_TO_LOW" })
-                }
-                checked={state.sortBy && state.sortBy === "PRICE_HIGH_TO_LOW"}
-              />{" "}
-              High To Low
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="sort"
-                value={state.sortBy}
-                onChange={() =>
-                  dispatch({ type: "SORT", payload: "PRICE_LOW_TO_HIGH" })
-                }
-                checked={state.sortBy && state.sortBy === "PRICE_LOW_TO_HIGH"}
-              />{" "}
-              Low To High
-            </label>
-          </div>
-
-          <div className="asideTitle">PRICE RANGE</div>
-          <label>
-            <input
-              className="slider"
-              type="range"
-              min="1200"
-              max="100000"
-              value={state.priceRange}
-              onChange={(e) =>
-                dispatch({ type: "PRICE_RANGE", payload: e.target.value })
-              }
-            />
-            Value : ₹{state.priceRange}
-          </label>
+          <Sidebar />
         </aside>
 
         <div className="cardParent">
