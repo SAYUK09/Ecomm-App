2f8c687cf7d72387085b58e1e297842cb25e0e15
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-05-30 20:45:44 +0530

    PR Chnages : Removed Unused Code, Comments

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@ import "./styles.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Router } from "./components/routes/Router";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-//   useNavigate,
-//   useParams,
-//   useLocation
-// } from "react-router-dom";
 import { useProductContext } from "./contexts/Products-Context";
 import { useCart } from "./contexts/Cart-Context";
-import { Products } from "./pages/products/Products";
-import { Cart } from "./pages/cart/Cart";
 import { Nav } from "./components/nav/Nav";
-import { Wishlist } from "./pages/wishlist/Wishlist";
-import { ProductDetail } from "./pages/product-details/ProductDetail";
 
 export default function App() {
   const { state, dispatch } = useProductContext();
   const { cartState, cartDispatch } = useCart();
-  console.log("paapapapapapapapapappp")
+
   useEffect(() => {
     (async function () {
       try {
