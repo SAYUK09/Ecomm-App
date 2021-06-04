aa2dd6e6e5c459a03a9eaef0e7d0440a4c8f91ae
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-04 19:47:51 +0530

    feat : Add to cart/wishlist only if logined

--- src/utilty/cart-utility.js
+++ src/utilty/cart-utility.js
@@ -1,101 +1,117 @@
 import axios from "axios";
 
 export function axiosAddToCart(prd, cartDispatch, auth) {
-  (async function () {
-    try {
-      const resp = await axios.post(
-        "https://ecom-backend-1.sayuk.repl.co/cart",
-        {
-          image: prd.image,
-          name: prd.name,
-          description: prd.description,
-          category: prd.category,
-          brand: prd.brand,
-          originalPrice: prd.originalPrice,
-          discountedPrice: prd.discountedPrice,
-          ratings: prd.ratings,
-          qty: prd.qty,
-          inStock: prd.inStock,
-          fastDelivery: prd.fastDelivery,
-        },
-        {
-          headers: {
-            "auth-token": auth.token,
+  if (auth) {
+    (async function () {
+      try {
+        const resp = await axios.post(
+          "https://ecom-backend-1.sayuk.repl.co/cart",
+          {
+            image: prd.image,
+            name: prd.name,
+            description: prd.description,
+            category: prd.category,
+            brand: prd.brand,
+            originalPrice: prd.originalPrice,
+            discountedPrice: prd.discountedPrice,
+            ratings: prd.ratings,
+            qty: prd.qty,
+            inStock: prd.inStock,
+            fastDelivery: prd.fastDelivery,
           },
-        }
-      );
+          {
+            headers: {
+              "auth-token": auth.token,
+            },
+          }
+        );
 
-      cartDispatch({ type: "ADD_TO_CART", payload: resp.data });
-    } catch (err) {
-      console.log(err);
-    }
-  })();
+        cartDispatch({ type: "ADD_TO_CART", payload: resp.data });
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  } else {
+    console.log("please log in");
+  }
 }
 
 export function axiosAddQty(prd, cartDispatch, auth) {
-  (async function () {
-    try {
-      const response = await axios.patch(
-        `https://ecom-backend-1.sayuk.repl.co/cart/${prd._id}`,
-        {
-          qty: prd.qty + 1,
-        },
-        {
-          headers: {
-            "auth-token": auth.token,
+  if (auth) {
+    (async function () {
+      try {
+        const response = await axios.patch(
+          `https://ecom-backend-1.sayuk.repl.co/cart/${prd._id}`,
+          {
+            qty: prd.qty + 1,
           },
-        }
-      );
-      console.log(response);
-      cartDispatch({ type: "LOAD_CART", payload: response.data });
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+          {
+            headers: {
+              "auth-token": auth.token,
+            },
+          }
+        );
+        console.log(response);
+        cartDispatch({ type: "LOAD_CART", payload: response.data });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  } else {
+    console.log("Please Login");
+  }
 }
 
 export function axiosDecrementQty(prd, cartDispatch, auth) {
-  (async function () {
-    if (prd.qty === 1) {
-      axiosRemoveFromCart(prd, cartDispatch);
-    }
+  if (auth) {
+    (async function () {
+      if (prd.qty === 1) {
+        axiosRemoveFromCart(prd, cartDispatch);
+      }
 
-    try {
-      const response = await axios.patch(
-        `https://ecom-backend-1.sayuk.repl.co/cart/${prd._id}`,
-        {
-          qty: prd.qty - 1,
-        },
-        {
-          headers: {
-            "auth-token": auth.token,
+      try {
+        const response = await axios.patch(
+          `https://ecom-backend-1.sayuk.repl.co/cart/${prd._id}`,
+          {
+            qty: prd.qty - 1,
           },
-        }
-      );
-      console.log(response.data, "cartUtlity 75");
-      cartDispatch({ type: "LOAD_CART", payload: response.data });
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+          {
+            headers: {
+              "auth-token": auth.token,
+            },
+          }
+        );
+        console.log(response.data, "cartUtlity 75");
+        cartDispatch({ type: "LOAD_CART", payload: response.data });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  } else {
+    console.log("Please Login");
+  }
 }
 
 export function axiosRemoveFromCart(prd, cartDispatch, auth) {
-  (async function () {
-    console.log(prd._id);
-    try {
-      const response = await axios.delete(
-        `https://ecom-backend-1.sayuk.repl.co/cart/${prd._id}`,
-        {
-          headers: {
-            "auth-token": auth.token,
-          },
-        }
-      );
+  if (auth) {
+    (async function () {
+      console.log(prd._id);
+      try {
+        const response = await axios.delete(
+          `https://ecom-backend-1.sayuk.repl.co/cart/${prd._id}`,
+          {
+            headers: {
+              "auth-token": auth.token,
+            },
+          }
+        );
 
-      cartDispatch({ type: "LOAD_CART", payload: response.data });
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+        cartDispatch({ type: "LOAD_CART", payload: response.data });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  } else {
+    console.log("Please Login");
+  }
 }
--- src/utilty/wishlist-utility.js
+++ src/utilty/wishlist-utility.js
@@ -1,53 +1,61 @@
 import axios from "axios";
 
 export function axiosRemoveFromWishlist(prd, setWishlistItems, auth) {
-  (async function () {
-    try {
-      const response = await axios.delete(
-        `https://ecom-backend-1.sayuk.repl.co/wishlist/${prd._id}`,
-        {
-          headers: {
-            "auth-token": auth.token,
-          },
-        }
-      );
+  if (auth) {
+    (async function () {
+      try {
+        const response = await axios.delete(
+          `https://ecom-backend-1.sayuk.repl.co/wishlist/${prd._id}`,
+          {
+            headers: {
+              "auth-token": auth.token,
+            },
+          }
+        );
 
-      console.log(response.data);
-      setWishlistItems(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+        console.log(response.data);
+        setWishlistItems(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  } else {
+    console.log("Please Login");
+  }
 }
 
 export function axiosAddToWishlist(prd, wishlistItems, setWishlistItems, auth) {
-  (async function () {
-    try {
-      const resp = await axios.post(
-        "https://ecom-backend-1.sayuk.repl.co/wishlist",
-        {
-          image: prd.image,
-          name: prd.name,
-          description: prd.description,
-          category: prd.category,
-          brand: prd.brand,
-          originalPrice: prd.originalPrice,
-          discountedPrice: prd.discountedPrice,
-          ratings: prd.ratings,
-          qty: prd.qty,
-          inStock: prd.inStock,
-          fastDelivery: prd.fastDelivery,
-        },
-        {
-          headers: {
-            "auth-token": auth.token,
+  if (auth) {
+    (async function () {
+      try {
+        const resp = await axios.post(
+          "https://ecom-backend-1.sayuk.repl.co/wishlist",
+          {
+            image: prd.image,
+            name: prd.name,
+            description: prd.description,
+            category: prd.category,
+            brand: prd.brand,
+            originalPrice: prd.originalPrice,
+            discountedPrice: prd.discountedPrice,
+            ratings: prd.ratings,
+            qty: prd.qty,
+            inStock: prd.inStock,
+            fastDelivery: prd.fastDelivery,
           },
-        }
-      );
+          {
+            headers: {
+              "auth-token": auth.token,
+            },
+          }
+        );
 
-      setWishlistItems([...wishlistItems, resp.data]);
-    } catch (err) {
-      console.log(err);
-    }
-  })();
+        setWishlistItems([...wishlistItems, resp.data]);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  } else {
+    console.log("Please Login");
+  }
 }
