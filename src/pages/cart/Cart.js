c40e5532ca79831efa113ceebc4c161d005e13cc
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-07-12 23:24:52 +0530

    Not Found svg displayed when cart is empty

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,9 +1,10 @@
 import "./Cart.css";
-import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useCart } from "../../contexts/Cart-Context";
 import { CartProductCard } from "../../components/cart-product-card/Cart-Product-Card";
 import { useAuth } from "../../contexts/Auth-Context";
 import axios from "axios";
+import emptySvg from "../../assets/Empty.svg";
 
 export function Cart() {
   const { cartState, cartDispatch } = useCart();
@@ -19,29 +20,40 @@ export function Cart() {
 
   return (
     <div className="cartParent">
+      {!cartState.cart.length && (
+        <div className="emptySvgDiv">
+          <img src={emptySvg} />
+          <h2>Nothing in Liked Videos</h2>
+          <Link className="emptySvgLink" to="/">
+            Shop Now
+          </Link>
+        </div>
+      )}
       <div className="cartPrdsContainer">
         {cartState.cart.map((prd) => {
           return <CartProductCard prd={prd} />;
         })}
       </div>
 
-      <div className="paymentContainer">
-        <div className="paymentSect">
-          <div className="paymentSectTitle">Total MRP</div>
-          <div className="paymentAmt">{getPrice()}</div>
-        </div>
-        {/*  */}
+      {cartState.cart.length && (
+        <div className="paymentContainer">
+          <div className="paymentSect">
+            <div className="paymentSectTitle">Total MRP</div>
+            <div className="paymentAmt">{getPrice()}</div>
+          </div>
+          {/*  */}
 
-        <div className="paymentSect">
-          <div className="paymentAmt">Total Discount</div>
-          <div className="paymentAmt discAmt">-₹0</div>
-        </div>
+          <div className="paymentSect">
+            <div className="paymentAmt">Total Discount</div>
+            <div className="paymentAmt discAmt">-₹0</div>
+          </div>
 
-        <div className="paymentSect paymentSectTotal">
-          <div className="paymentAmt">Total Amount</div>
-          <div className="paymentAmt ">{getPrice()}</div>
+          <div className="paymentSect paymentSectTotal">
+            <div className="paymentAmt">Total Amount</div>
+            <div className="paymentAmt ">{getPrice()}</div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
